05d103588b5bbfc689669e22fd84a0cf22f67dc7
Adrián Vegas <136771846+adriansvegas@users.noreply.github.com>
2025-11-23 00:21:29 -0400

    Configuración del Main

--- PROYECTO_SO2/src/proyecto_so2/Main.java
+++ PROYECTO_SO2/src/proyecto_so2/Main.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyecto_so2;
-
+import Controlador.SimuladorFS;
+import vista.VentanaPrincipal;
+import javax.swing.SwingUtilities;
+// import so_operativos.Logger;
 /**
  *
  * @author Edgar
@@ -12,6 +15,15 @@ package proyecto_so2;
 
 public class Main {
     public static void main(String[] args) {
-       
+       // Opcional: Inicializar logger
+        // Logger.init(); 
+        
+        System.out.println("Iniciando Sistema de Archivos...");
+        
+        SwingUtilities.invokeLater(() -> {
+            SimuladorFS modelo = new SimuladorFS();
+            VentanaPrincipal vista = new VentanaPrincipal(modelo);
+            vista.setVisible(true);
+        });
     }
 }
